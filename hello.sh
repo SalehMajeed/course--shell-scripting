6654ce0cfb2ffb9eaff5873161001b745931208e
salehmajeed <saleh.majeed8@gmail.com>
2022-09-15 02:36:32 +0530

    shell commands

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# this is the comment
+echo "Hello World" # this is also the comment
+
+# System variables
+echo Our shell name is $BASH
+echo Our shell version name is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our current working directory is $PWD
+
+# User Variables
+name= BASH
+VAL=10
+echo The name is $name
+echo The value is $VAL
